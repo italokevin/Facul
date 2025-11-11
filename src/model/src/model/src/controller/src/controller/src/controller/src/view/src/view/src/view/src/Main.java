32b14784f641594a6f04443228598ad0a470cccb
italokevin <italokevin28@gmail.com>
2025-11-11 16:50:42 -0300

    Adiciona classe Main

---
+++ src/model/src/model/src/controller/src/controller/src/controller/src/view/src/view/src/view/src/Main.java
@@ -0,0 +1,18 @@
+import view.LoginView;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        SwingUtilities.invokeLater(() -> {
+            LoginView login = new LoginView();
+            login.setVisible(true);
+        });
+    }
+}
